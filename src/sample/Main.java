86bc1e6122a2b56652eda1d08a2c5d9fcf4ed7b6
oussamabonnor1 <oussamabonnor@gmail.com>
2019-06-30 15:50:04 +0100

    adding images, working on search bar and options bar (design)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../Views/homeview.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
 
