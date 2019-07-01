2725d15238bf1d32cc46b5aadcfe2caaadb4f08c
oussamabonnor1 <oussamabonnor@gmail.com>
2019-07-01 12:28:14 +0100

    adding resources and view for the custom cell of friends

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../Views/homeview.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../Views/home_view.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
