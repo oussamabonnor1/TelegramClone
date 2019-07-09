03561364cdaaafa45b6cd45753648d6df2540627
oussamabonnor1 <oussamabonnor@gmail.com>
2019-07-09 01:06:42 +0100

    adding closing and minimizing functions

--- src/Controllers/LogInController.java
+++ src/Controllers/LogInController.java
@@ -1,24 +1,33 @@
 package Controllers;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LogInController {
+public class LogInController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
 
     @FXML
     void closeApp(MouseEvent event) {
-
+        Main.stage.close();
     }
 
     @FXML
     void minimizeApp(MouseEvent event) {
-
+        Main.stage.setIconified(true);
     }
 
     @FXML
@@ -30,4 +39,5 @@ public class LogInController {
             e.printStackTrace();
         }
     }
+
 }
